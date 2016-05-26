d7dde4ea5b12d6672c8d06468a658f6ce891df76
daxinman <dax.inman@gmail.com>
2016-05-26 16:11:13 -0700

    brute force

---
+++ problem_9.cpp
@@ -0,0 +1,26 @@
+// Project Euler
+// Problem 8
+
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main()
+{
+	double a, b, c;
+
+	for (c = 1; c < 500; c++)
+	{
+		for (b = 1; b < 500; b++)
+		{
+			for (a = 1; a < 500; a++)
+			{
+				if (pow(a, 2) + pow(b, 2) == pow(c, 2)  && ((a + b + c) == 1000))  
+				{
+					cout << a << " " << b << " " << c << endl;
+					unsigned long long answer= a*b*c;
+					cout << answer << endl;
+				}
+			}
+		}
+	}
+}
